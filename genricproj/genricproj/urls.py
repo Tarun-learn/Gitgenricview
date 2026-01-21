07ad2d5b7cdd61a175bbba36955d8efce5407e38
Tarun-learn <116tarun@gmail.com>
2026-01-21 19:31:38 +0530

    sdsd

--- genricproj/genricproj/urls.py
+++ genricproj/genricproj/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.http import HttpResponse
+
+def home (request):
+    return HttpResponse("Django is ranning")
 
 urlpatterns = [
+    path('', home),           #/route
     path('admin/', admin.site.urls),
     path('', include('genricapp.urls')),
     
